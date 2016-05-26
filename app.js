fd00fcb422477b5314d905065712c92a3fa7437f
Mickey <develop.agility@gmail.com>
2016-05-26 11:53:04 -0600

    gonna merge it

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1', api);
 
-app.use('*', (req, res, next) =>{
-  res.sendFile('index.html', {
-    root: __dirname + '/public/'
-  });
-});
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
